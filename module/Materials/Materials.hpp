6a352e9bbe816c7729a4f85ec4e113e2e190d252
keporusu <kepokeke1059@gmail.com>
2026-03-09 22:24:35 +0900

    cornell-box追加

--- module/Materials/Materials.hpp
+++ module/Materials/Materials.hpp
@@ -4,28 +4,32 @@
 #include <iostream>
 #include <string>
 
-#define MATERIAL_LAMBERTIAN 1
-#define MATERIAL_METAL 2
-#define MATERIAL_DIELECTRIC 3
+#define MATERIAL_LAMBERTIAN 0
+#define MATERIAL_METAL 1
+#define MATERIAL_DIELECTRIC 2
+#define MATERIAL_DIFFUSE_LIGHT 10
 
 struct Material
 {
     int material_type;
     glm::vec3 albedo;
-    float fuzz;                // ぼやかし
-    float refraction_index;    // 相対屈折率
-    std::string texture_label; // テクスチャのラベル
+    float fuzz;                         // ぼやかし
+    float refraction_index;             // 相対屈折率
+    std::string texture_label;          // テクスチャのラベル
+    glm::vec3 emitted = glm::vec3(0.0); // 発光
 };
 
 struct SubUBO_Material
 {
-    int material_type;      // offset 4の倍数
-    float _padding0[3];     // パディング
-    glm::vec3 albedo;       // offset 16の倍数
-    float fuzz;             // offset 4の倍数
-    float refraction_index; // offset 4の倍数
-    int texture = -1;       // offset 4の倍数
-    float _padding1[2];     // 構造体のサイズを16の倍数に(48)
+    int material_type;      // 4
+    float _padding0[3];     // 16
+    glm::vec3 albedo;       // 28
+    float fuzz;             // 32
+    float refraction_index; // 36
+    int texture = -1;       // 40
+    float _padding1[2];     // 48
+    glm::vec3 emitted;      // 60
+    float _padding2;        // 64
 };
 
 struct UBO_Materials
